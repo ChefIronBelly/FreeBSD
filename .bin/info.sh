75bae3073dd30b401144f797e1becdcd197fcf19
ChefIronBelly <werthmation@gmail.com>
2024-01-11 19:52:35 -0500

    adds and tweaks

--- .bin/info.sh
+++ .bin/info.sh
@@ -16,7 +16,7 @@ f2="\e[0;37m"
 # or use wm array -- add any that need to be recognized
 wms=( 2bwm 2wm 9wm aewm afterstep ahwm alopex amiwm antiwm awesome blackbox bspwm catwm clfswm ctwm cwm dminiwm dragonflywm dwm echinus \
     euclid-wm evilpoison evilwm fluxbox flwm fvwm-crystal goomwwm hcwm herbstluftwm i3 icewm jwm karmen larswm lwm matwm2 mcwm monsterwm \
-    musca notion nwm olwm openbox oroborus pekwm ratpoison sapphire sawfish sscrotwm sithwm smallwm snapwm spectrwm stumpwm subtle swm tfwm tinywm tritium twm \
+    musca notion nwm olwm openbox oroborus pekwm ratpoison sapphire sawfish sithwm smallwm snapwm sowm spectrwm sscrotwm stumpwm subtle swm tfwm tinywm tritium twm \
     uwm vtwm w9wm weewm wind windowlab wm2 wmaker wmfs wmii wmx xfwm4 xmonad xoat yeahwm )
 
 color-echo() {  # print with colors
@@ -56,7 +56,7 @@ print-disk() {
 }
 
 print-mem() {
-	# requires freecolor Im lazy today.
+	# requires freecolor.
 	totalmem=$( freecolor -mo | awk 'NR==2 {print substr($2,0,4)}')
 	usedmem=$( freecolor -mo | awk 'NR==2 {print substr($3,0,3)}')
 	mem="${usedmem}MB / ${totalmem}MB"
@@ -65,19 +65,19 @@ print-mem() {
 
 print-wm() {
     for wm in ${wms[@]}; do          # pgrep through wmname array
-        pid=$(pgrep -x -u $USER $wm)		# if found, this wmname has running process
+        pid=$(pgrep -x -u $USER $wm) # if found, this wmname has running process
         if [[ "$pid" ]]; then
             color-echo 'WM' '       '"$wm"
 	        break
         fi
     done
-    #color-echo 'WM' '       '"Not Found / wmutils active"
+    color-echo 'WM' '       '"sowm" # fix me
 }
            
 print-font() {
     fontstr=$(xrdb -query 2>/dev/null | grep '*.font:')
-    font=$(echo $fontstr | awk -F: '{ print $2 }')
-    [[ $font != "" ]] && color-echo 'FONT' '    '"$font"
+    font=$(echo $fontstr | awk -F: '{ print $3 }')
+    [[ $font != "" ]] && color-echo 'FONT' '    '" $font"
 }
 
 print-distro() {
